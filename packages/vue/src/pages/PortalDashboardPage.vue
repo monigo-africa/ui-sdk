315aeb02945815ac04a4742f9955027f8ee3618c
akachukwu-shuttlers <okafor.ak@outlook.com>
2026-04-18 23:18:02 +0100

    feat(vue): dashboard, invoices, and invoice-detail page components

---
+++ packages/vue/src/pages/PortalDashboardPage.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import type { PortalInvoice, Bill } from '@monigo/portal-core'
+import DashboardSummary from '../components/DashboardSummary.vue'
+import RecentActivity from '../components/RecentActivity.vue'
+
+interface Props { class?: string }
+const props = withDefaults(defineProps<Props>(), { class: '' })
+const emit = defineEmits<{
+  invoiceClick: [inv: PortalInvoice]
+  billClick: [bill: Bill]
+}>()
+</script>
+
+<template>
+  <section :class="['monigo-page', props.class]">
+    <DashboardSummary />
+    <RecentActivity
+      @invoice-click="(inv) => emit('invoiceClick', inv)"
+      @bill-click="(bill) => emit('billClick', bill)"
+    />
+  </section>
+</template>
---
+++ packages/vue/src/pages/PortalInvoiceDetailPage.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import InvoiceDetail from '../components/InvoiceDetail.vue'
+
+interface Props {
+  invoiceId: string
+  class?: string
+}
+const props = withDefaults(defineProps<Props>(), { class: '' })
+</script>
+
+<template>
+  <InvoiceDetail :invoice-id="props.invoiceId" :class="props.class" />
+</template>
